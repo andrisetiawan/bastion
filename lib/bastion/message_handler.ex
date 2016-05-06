901f310e411d7e2df8482af8391ba1cb410f6417
Daniel Grieve <git@cazrin.net>
2016-05-06 14:51:40 +0100

    Handle name postback

--- lib/bastion/message_handler.ex
+++ lib/bastion/message_handler.ex
@@ -12,6 +12,13 @@ defmodule Bastion.MessageHandler do
     send_button_message(msg.sender.id, "Choose from the following options", buttons)
   end
 
+  def handle(msg = %{postback: %{payload: "PB_NAME"}}) do
+    send_text_message(
+      msg.sender.id,
+      "My name is SST Laboratories Siege Automation E54, otherwise known as Bastion"
+    )
+  end
+
   def handle(msg) do
     Logger.info "Unhandled message:\n#{inspect msg}"
   end
@@ -30,6 +37,20 @@ defmodule Bastion.MessageHandler do
             buttons: buttons
           }
         }
+      }
+    }
+
+    url = "https://graph.facebook.com/v2.6/me/messages?access_token=#{@fb_page_access_token}"
+    headers = [{"Content-Type", "application/json"}]
+    HTTPoison.post!(url, Poison.encode!(payload), headers)
+  end
+
+  defp send_text_message(recipient, text) do
+    payload = %{
+      recipient: %{id: recipient},
+
+      message: %{
+        text: text
       }
     }
 
